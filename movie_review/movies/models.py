dc7a1a00e2c0c1060ba2c988564a50d7c36e876f
dangyulee <dg200101@gmail.com>
2025-07-14 11:21:44 +0900

    usermodel

--- movie_review/movies/models.py
+++ movie_review/movies/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class exam_model(models.Model):
+    user = models.ForeignKey(CustomUser, null=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     body = models.TextField(default="default")
     date = models.DateTimeField(auto_now_add=True)

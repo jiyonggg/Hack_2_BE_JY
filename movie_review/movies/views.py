0502fbb6da89eeb265d52233809277efdc4c56e8
jiyonggg <kimvergognoso21@hufs.ac.kr>
2025-07-14 16:21:21 +0900

    feat. Initialize database from external data

--- movie_review/movies/views.py
+++ movie_review/movies/views.py
@@ -1,3 +1,101 @@
 from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import requests
+
+from . import serializers
+from . import models
 
 # Create your views here.
+def init_db(request):
+    '''
+    데이터베이스에 외부 영화 정보 저장
+    '''
+    url = 'http://43.200.28.219:1313/movies/'
+    res = requests.get(url)
+    movies = res.json()['movies']
+    for movie in movies:
+        # movie: 영화 하나
+        # json 내 key와 영화 모델의 key 매핑 딕셔너리
+        matches = {
+            'title_kor': 'title_kor',
+            'title_eng': 'title_ori',
+            'poster_url': 'poster_url',
+            'release_date': 'release_date',
+            'rating': 'rate',
+            'genre': 'genre',
+            'showtime': 'showtime',
+            'plot': 'plot',
+            'actors': 'actors',
+
+            # 감독을 미리 cast로 넣기 위한 처리
+            'director_name': 'name',
+            'director_image_url': 'image_url',
+        }
+        
+        # 추가할 데이터 정보 담는 딕셔너리
+        data = dict()
+
+        for key in movie.keys():
+            if key == 'rating': # float 타입 rating 처리
+                data[matches[key]] = float(movie.get(key, ''))
+            elif key == 'showtime': # int 타입 showtime 처리
+                data[matches[key]] = int(movie.get(key, ''))
+            else: # 나머지 데이터 처리
+                data[matches[key]] = movie.get(key, '')
+        
+        director_info = {'character': '감독'}
+        
+        for key in ('name', 'image_url'):
+            director_info[key] = data.pop(key)
+        
+        casts = [director_info] + data.pop('actors')
+
+        instance = models.Movie.objects.create(**data)
+
+        for cast in casts:
+            cast_info = dict()
+            cast_info['name'] = cast.get('name', '')
+            cast_info['profile_url'] = cast.get('image_url', '')
+            cast_info['role'] = cast.get('character', '')
+            cast_info['movie_id'] = instance
+            models.Cast.objects.create(**cast_info)
+    
+
+class MovieList(APIView):
+    '''
+    모든 영화 목록을 조회
+    '''
+    def get(self, request):
+        movies = models.Movie.objects.all()
+        serializer = serializers.MovieListResponseSerializer(movies, many=True)
+        return Response(serializer.data)
+
+class MovieDetail(APIView):
+    '''
+    한 영화의 상세 정보를 조회
+    '''
+    def get(self, request, movie_id):
+        try:
+            movie = models.Movie.objects.get(id=movie_id)
+        except models.Movie.DoesNotExist:
+            raise Http404
+        serializer = serializers.MovieDetailResponseSerializer(models.Movie, many=True)
+
+class CommentList(APIView):
+    '''
+    한 영화에 대한 댓글 관련
+    '''
+    def get(self, request):
+        '''
+        모든 댓글 목록 조회
+        '''
+        pass
+
+    def post(self, request):
+        '''
+        댓글 작성
+        '''
+        pass

def1dce265ca07f63e9e33eeaf2eb9ee24d475df
jiyonggg <kimvergognoso21@hufs.ac.kr>
2025-07-14 17:10:19 +0900

    Add urls.py

---
+++ movie_review/movies/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+app_name = 'movies'
+urlpatterns = [
+    path('', views.MovieList.as_view(), name='movie-list'),
+    path('<int:movie_id>', views.MovieDetail.as_view(), name='movie-detail'), 
+    # path('<int:movie_id>/comments/', views.M=)
+]
+
+# <타입: 매개변수 이름>

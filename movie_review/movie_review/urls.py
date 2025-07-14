1a0b4a9dd259ed9b7c097703c62063fc2868e790
jiyonggg <kimvergognoso21@hufs.ac.kr>
2025-07-14 16:42:35 +0900

    feat. include movies urls

--- movie_review/movie_review/urls.py
+++ movie_review/movie_review/urls.py
@@ -21,4 +21,5 @@ from accounts.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('movies/', include('movies.urls')),
 ]

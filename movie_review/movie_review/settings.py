2509bc00dc3ebb11d7e856dfea4b950c9bee4f00
dangyulee <dg200101@gmail.com>
2025-07-15 14:41:46 +0900

    pagenation 수정

--- movie_review/movie_review/settings.py
+++ movie_review/movie_review/settings.py
@@ -186,7 +186,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 20
+    # 페이지네이션
 }
 
 from datetime import timedelta
